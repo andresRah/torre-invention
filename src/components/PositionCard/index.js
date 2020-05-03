e7592b84c02781e0064b5d8539942a113b454d45
Andres Arevalo <andres.arevalopar@gmail.com>
2020-05-03 00:53:58 -0500

    Cards handling behavior and add-hook to control loading cards

---
+++ src/components/PositionCard/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Article, ImgWrapper, Img } from './styles'
+import { useNearScreen } from '../../hooks/useNearScreen.js'
+import { Link } from '@reach/router'
+
+const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
+  const [show, element] = useNearScreen()
+
+  return (
+    <Article ref={element}>
+      {show && (
+        <>
+          <Link to={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </Link>
+        </>
+      )}
+    </Article>
+  )
+}
